63419794d5004b0e8053b9fb0e6e99a770ea0d04
Diogo Soares <32431609+DiogoSoaress@users.noreply.github.com>
2025-04-11 14:15:18 +0200

    feat: Safenet page

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -16,10 +16,13 @@ export const GITHUB_LINK = 'https://github.com/safe-global'
 export const TWITTER_LINK = 'https://x.com/safe'
 export const YOUTUBE_LINK = 'https://www.youtube.com/@safe'
 export const SAFE_NETWORK_SUPPORT = 'https://docs.safe.global/core-api/safe-infrastructure-deployment'
+export const SAFENET_TELEGRAM_LINK = 'https://t.me/+jHGVUd8y1FQxMzJi'
+export const SAFENET_WAITING_LIST_LINK = 'https://wn2n6ocviur.typeform.com/to/I3GDiH2N'
 
 export const DOCS_SDK = 'https://docs.safe.global/sdk/overview'
 export const DOCS_INFRASTRUCTURE = 'https://docs.safe.global/core-api/api-overview'
 export const DOCS_SMART_CONTRACTS = 'https://docs.safe.global/advanced/smart-account-overview'
+export const DOCS_SAFENET = 'https://docs.safe.global/safenet/overview'
 
 // Mobile apps
 export const IOS_LINK = 'https://apps.apple.com/app/id1515759131'
